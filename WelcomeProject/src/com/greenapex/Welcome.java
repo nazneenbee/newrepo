02042408ff7f1b48cb2a032d4f6ca39a1f32e950
nazneenbee <beenazneen@gmail.com>
2021-09-13 14:44:57 +0530

    again change file

--- WelcomeProject/src/com/greenapex/Welcome.java
+++ WelcomeProject/src/com/greenapex/Welcome.java
@@ -5,14 +5,6 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
-		System.out.println("hii this first git commit");
-		System.out.println("hiii");
-        System.out.println("hello");
-        System.out.println("hello git");
-        System.out.println("hii this first git commit");
-		System.out.println("hiii");
-        System.out.println("hello");
-        System.out.println("hello git");
 	}
 
 }
