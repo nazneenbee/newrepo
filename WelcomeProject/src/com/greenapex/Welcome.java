982f2fb927066a6081ec61d960fd916322c27059
nazneenbee <beenazneen@gmail.com>
2021-09-14 09:22:15 +0530

    push in main

--- WelcomeProject/src/com/greenapex/Welcome.java
+++ WelcomeProject/src/com/greenapex/Welcome.java
@@ -3,6 +3,7 @@ package com.greenapex;
 public class Welcome {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to main");
 
 		System.out.println("Welcome to branch1");
 		System.out.println("Welcome to branch1");
