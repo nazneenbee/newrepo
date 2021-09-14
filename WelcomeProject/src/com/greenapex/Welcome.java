35d3a174214ab1be8e498aea6418e02c03279f54
nazneenbee <beenazneen@gmail.com>
2021-09-14 10:45:34 +0530

    changes in main

--- WelcomeProject/src/com/greenapex/Welcome.java
+++ WelcomeProject/src/com/greenapex/Welcome.java
@@ -3,8 +3,7 @@ package com.greenapex;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to main");
-		System.out.println("Welcome to main");
+		
 
 		
 	}
