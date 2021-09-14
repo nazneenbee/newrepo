1562946cc0b4690b0840e4a8f02970f0f620cb00
nazneenbee <beenazneen@gmail.com>
2021-09-14 11:21:58 +0530

    only branch2 msg

--- WelcomeProject/src/com/greenapex/Welcome.java
+++ WelcomeProject/src/com/greenapex/Welcome.java
@@ -3,12 +3,7 @@ package com.greenapex;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to main");
-
-		System.out.println("Welcome to branch1");
-		System.out.println("Welcome to branch1");
-
-		// TODO Auto-generated method stub
+		
 		
 		System.out.println("Welcome to branch2");
 		System.out.println("Welcome to branch2");
